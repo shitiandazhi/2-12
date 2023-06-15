fbfc62970744aa3b1a7c998f26fd8f6ccaf0bdd0
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-06-15 06:36:24 +0000

    [finish]2-12

--- db/migrate/20230610074601_create_books.rb
+++ db/migrate/20230610074601_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :book do |t|
-      t.string :本のタイトル
-      t.string :感想
+      t.string :title
+      t.string :body
       t.timestamps
     end
   end
